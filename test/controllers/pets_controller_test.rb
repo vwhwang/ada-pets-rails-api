6c564feae9cf9fab1314d380e1263f0a09af937d
CheezItMan <chris@adadevelopersacademy.org>
2020-05-20 19:34:50 -0700

    added 1st index action test

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -2,8 +2,10 @@ require "test_helper"
 
 describe PetsController do
   it "must get index" do
-    get pets_index_url
+    get pets_path
+
     must_respond_with :success
+    expect(response.header['Content-Type']).must_include 'json'
   end
 
 end

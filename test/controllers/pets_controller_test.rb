6cf44c635e2e1e47c67a963f50c7c2ee4e15bfaa
dee del rosario <dee@adadevelopersacademy.org>
2020-05-21 10:03:27 -0700

    refactors pet controller test with check_response helper

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -2,29 +2,35 @@ require "test_helper"
 
 describe PetsController do
 
+  REQUIRED_PET_FIELDS = ["id", "name", "species", "age", "owner"].sort
+
+  def check_response(expected_type:, expected_status: :success)
+    must_respond_with expected_status
+    expect(response.header['Content-Type']).must_include 'json'
+
+    body = JSON.parse(response.body)
+    expect(body).must_be_kind_of expected_type
+    return body
+  end
+
   describe "index" do
     it "responds with JSON and success" do
       get pets_path
 
-      expect(response.header['Content-Type']).must_include 'json'
-      must_respond_with :ok
+      check_response(expected_type: Array)
     end
 
     it "responds with an array of pet hashes" do
       # Act
       get pets_path
 
-      # Get the body of the response
-      body = JSON.parse(response.body)
-
       # Assert
-      expect(body).must_be_instance_of Array
+      body = check_response(expected_type: Array)
+
       body.each do |pet|
         expect(pet).must_be_instance_of Hash
 
-        required_pet_attrs = ["id", "name", "species", "age", "owner"]
-
-        expect(pet.keys.sort).must_equal required_pet_attrs.sort
+        expect(pet.keys.sort).must_equal REQUIRED_PET_FIELDS
       end
     end
 
@@ -34,10 +40,9 @@ describe PetsController do
 
       # Act
       get pets_path
-      body = JSON.parse(response.body)
 
       # Assert
-      expect(body).must_be_instance_of Array
+      body = check_response(expected_type: Array)
       expect(body).must_equal []
     end
   end
@@ -59,7 +64,7 @@ describe PetsController do
         post pets_path, params: pet_data
       }.must_differ "Pet.count", 1
 
-      must_respond_with :created
+      check_response(expected_type: Hash, expected_status: :created)
     end
 
     it "will respond with bad_request for invalid data" do
@@ -76,10 +81,7 @@ describe PetsController do
       # Assert
       }.wont_change "Pet.count"
     
-      must_respond_with :bad_request
-
-      expect(response.header['Content-Type']).must_include 'json'
-      body = JSON.parse(response.body)
+      body = check_response(expected_type: Hash, expected_status: :bad_request)
       expect(body["errors"].keys).must_include "age"
     end
 

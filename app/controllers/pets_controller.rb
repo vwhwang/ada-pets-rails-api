65827378ae578914e1b73f668ca9e31626751221
vwhwang <vwhwang38@gmail.com>
2020-05-26 11:02:52 -0700

    showed index and tests passed for pet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,6 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all
-    render json: {does_it_work: "yep"}, status: :ok
+    pets = Pet.all
+    render json: pets.as_json(only: [:id,:name,:age,:species,:owner]), status: :ok
   end
 end
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -11,8 +11,33 @@ describe PetsController do
   end
 
   it "will return proper fields for a list of pets" do 
+
     pet_fields = ["id","name","age","owner","species"].sort 
+
+    get pets_path 
+
+    body = JSON.parse(response.body)
+
+    expect(body).must_be_instance_of Array
+
+    body.each do |pet|
+      expect(pet).must_be_instance_of Hash
+      expect(pet.keys.sort).must_equal pet_fields
+    end 
   
+  end 
+
+
+  it "will show empty array if no pets" do 
+
+    Pet.destroy_all
+
+    get pets_path 
+
+    body = JSON.parse(response.body)
+
+    expect(body).must_be_instance_of Array
+    expect(body.length).must_equal 0 
   end 
 
 end
